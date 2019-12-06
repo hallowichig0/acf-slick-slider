2139417a67dd1b31a3a8bc4b747af8e15914c0e1
Jayson Garcia <garciajayson502@gmail.com>
2019-12-06 15:20:05 +0800

    fixed enqueue path location for js & css.

--- acf_slick_slider.php
+++ acf_slick_slider.php
@@ -27,12 +27,12 @@ add_action('init', 'acf_slick_slider');
 function acf_slick_slider_enqueue() {
 
     // styles
-    wp_enqueue_style( 'aaslider-slick-style', ASSlider__PLUGIN_URL . '/assets/slick/slick.css' );
+    wp_enqueue_style( 'aaslider-slick-style', ASSlider__PLUGIN_URL . 'assets/slick/slick.css' );
     wp_enqueue_style( 'asslider-style', ASSlider__PLUGIN_URL . 'acf_slick_slider.css' );
 
     // scripts
-    wp_enqueue_script( 'aaslider-slick-script', ASSlider__PLUGIN_URL . '/assets/slick/slick.js','','',true  );
-    wp_enqueue_script( 'asslider-js', ASSlider__PLUGIN_URL . '/js/acf_slick_slider.js','','',true );
+    wp_enqueue_script( 'aaslider-slick-script', ASSlider__PLUGIN_URL . 'assets/slick/slick.js','','',true  );
+    wp_enqueue_script( 'asslider-js', ASSlider__PLUGIN_URL . 'js/acf_slick_slider.js','','',true );
 
 }
 add_action( 'wp_enqueue_scripts', 'acf_slick_slider_enqueue' );
